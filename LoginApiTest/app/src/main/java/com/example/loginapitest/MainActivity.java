c7ebbb85c11692365823d65ac3a0f719a283c85c
godol811 <32415176+godol811@users.noreply.github.com>
2020-12-23 15:34:10 +0900

    Add files via upload
    귀찮아

---
+++ LoginApiTest/app/src/main/java/com/example/loginapitest/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.loginapitest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nhn.android.naverlogin.OAuthLogin;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    
+
+    }
+}
